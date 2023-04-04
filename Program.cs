16d04406b268a56a2e54f685b8841a8ca0618095
Jakub Berthoty <jakub.berthoty@gmail.com>
2023-04-04 13:07:21 +0200

    Implemented CRUD.

--- Program.cs
+++ Program.cs
@@ -1,5 +1,7 @@
 using Microsoft.EntityFrameworkCore;
 
+int currentId = 0;
+
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddDbContext<AttendanceDb>(options => options.UseInMemoryDatabase("attendancedb"));
@@ -9,11 +11,47 @@ var app = builder.Build();
 
 var group = app.MapGroup("attendance");
 
-group.MapPost("/", () => "New attendee added.");
-group.MapPut("{id}", (long id) => $"Attendee {id} updated.");
-group.MapGet("{id}", (long id) => $"Attendee {id}.");
-group.MapGet("", () => "List of all attendees.");
-group.MapDelete("{id}", (long id) => $"Attendee {id} deleted.");
+group.MapPost("/", async (Attendee attendee, AttendanceDb db) =>
+{
+    attendee.Id = ++currentId;
+    await db.AddAsync(attendee);
+    await db.SaveChangesAsync();
+
+    return Results.Created($"attendance/{attendee.Id}", new { attendee.Id });
+});
+group.MapPut("{id}", async (long id, Attendee attendee, AttendanceDb db) =>
+{
+    attendee.Id = id;
+    db.Update(attendee);
+
+    await db.SaveChangesAsync();
+
+    return Results.NoContent();
+});
+group.MapGet("{id}", (long id, AttendanceDb db) =>
+{
+    var savedAttendee = db.Attendees.SingleOrDefault(att => att.Id == id);
+    if (savedAttendee == null)
+    {
+        return Results.NotFound();
+    }
+    else
+    {
+        return Results.Ok(savedAttendee);
+    }
+});
+group.MapGet("", (AttendanceDb db) => Results.Ok(db.Attendees.ToArray()));
+group.MapDelete("{id}", async (long id, AttendanceDb db) =>
+{
+    var savedAttendee = db.Attendees.SingleOrDefault(att => att.Id == id);
+    if (savedAttendee != null)
+    {
+        db.Attendees.Remove(savedAttendee);
+        await db.SaveChangesAsync();
+    }
+
+    return Results.NoContent();
+});
 
 app.Run();
 
