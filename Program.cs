896c561d140975966b72a9261e2d58695a15babc
Jakub Berthoty <jakub.berthoty@gmail.com>
2023-04-04 10:36:12 +0200

    CRUD endpoint placeholders.

--- Program.cs
+++ Program.cs
@@ -1,6 +1,10 @@
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+app.MapPost("attendance", () => "New attendee added.");
+app.MapPut("attendance/{id}", (long id) => $"Attendee {id} updated.");
+app.MapGet("attendance/{id}", (long id) => $"Attendee {id}.");
+app.MapGet("attendance", () => "List of all attendees.");
+app.MapDelete("attendance/{id}", (long id) => $"Attendee {id} deleted.");
 
 app.Run();
